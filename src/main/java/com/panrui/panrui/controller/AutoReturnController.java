b9a895fa9b71ee020efd71b4a6a605d57187b115
panrui <panruihero@163.com>
2020-07-29 18:11:02 +0800

    updata-2020-0729-18:10

--- src/main/java/com/panrui/panrui/controller/AutoReturnController.java
+++ src/main/java/com/panrui/panrui/controller/AutoReturnController.java
@@ -4,19 +4,16 @@ import com.panrui.panrui.bean.User;
 import com.panrui.panrui.mapper.UserLoginRepository;
 import com.panrui.panrui.service.UserLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.health.SystemHealth;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 
 @Controller
-public class AutoLoginController {
+public class AutoReturnController {
 
     @Autowired
     private UserLoginRepository userRepository;
@@ -32,7 +29,6 @@ public class AutoLoginController {
     @PostMapping(value = "/user/login")
     public String LoginIn(User user, Map<String,Object> mapMessage , HttpSession httpSession){
         User loginInState = userLoginService.doLoginIn(user.getUsername(),user.getPassword());
-        System.out.println("断点1");
         if(loginInState!=null){
             httpSession.setAttribute("loginUser" , loginInState);
             return "redirect:/main.html";
